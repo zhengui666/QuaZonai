941c42b050dc85fcc09d61d2bed837bf14f75ad3
Zheng UI <aa64966298@163.com>
2026-08-31 20:02:12 +0800

    chore(ios): remove duplicate materialization artifacts

--- ios/Issue36Packages/QuaZonaiAPI/Sources/QuaZonaiAPI/Exports.swift
+++
@@ -1,7 +0,0 @@
-@_exported import OpenAPIRuntime
-@_exported import OpenAPIURLSession
-
-/// Marker exported by the generated wire-contract package.
-public enum QuaZonaiWireContract {
-    public static let capabilityEpoch = 1
-}
--- ios/Issue36Packages/QuaZonaiAPI/Tests/QuaZonaiAPITests/WireContractTests.swift
+++
@@ -1,7 +0,0 @@
-import Testing
-@testable import QuaZonaiAPI
-
-@Test("Generated client package exports the capability epoch")
-func capabilityEpoch() {
-    #expect(QuaZonaiWireContract.capabilityEpoch == 1)
-}
