c85e4bfc488138c7e26d7759408349b0532be2d7
Zheng UI <aa64966298@163.com>
2026-08-31 19:55:45 +0800

    build(ios): export generated wire contract

---
+++ ios/Issue36Packages/QuaZonaiAPI/Sources/QuaZonaiAPI/Exports.swift
@@ -0,0 +1,7 @@
+@_exported import OpenAPIRuntime
+@_exported import OpenAPIURLSession
+
+/// Marker exported by the generated wire-contract package.
+public enum QuaZonaiWireContract {
+    public static let capabilityEpoch = 1
+}
